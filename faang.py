b9a5fbba335edeb6f1fce6caaa61e861e8f6ddb3
KaiiMenai <kyra.menai.hamilton:outlook.com>
2025-09-30 18:53:20 +0100

    moved faang into the ci file

---
+++ faang.py
@@ -0,0 +1,17 @@
+# This program will contain the code to import and plot FAANG data.
+# This is for the Computer Infrastructure course.
+
+## Author: Kyra Menai Hamilton
+## Date: 30/09/2025
+## Version: 1.0
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import yfinance as yf
+import datetime
+
+# Next need to define the FAANG stocks to make it clear in the CSV file.
+
+# END
